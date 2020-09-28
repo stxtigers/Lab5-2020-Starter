eca12229078f7b2b4b23fed4bf8215038dba6b6d
Dan Able <abled@stxtigers.com>
2020-09-28 14:15:06 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+//Use this main class & method to crate a "tester class" for StringTools.java (complete that first)
+//A tester class runs each method in your StringTools class to verify they work
+//Once everyone is to this point we will write part of this class together, but if you get here early, see what you can figure out
+class Main {
+  public static void main(String[] args) {    
+    StringTools st = new StringTools();
+    
+
+  }
+}
---
+++ StringTools.java
@@ -0,0 +1,57 @@
+//NOTE #1: ALL METHODS SHOULD BE PUBLIC
+//NOTE #2: NO SCANNER SHOULD BE CREATED IN THIS CLASS
+
+public class StringTools{
+  /*Write a method padString that accepts two parameters: a String and an integer representing a length. The method should pad the parameter string with spaces until its length is the given length. For example, padString("hello", 8) should return "   hello". (This sort of method is useful when trying to print output that lines up horizontally.) If the string's length is already at least as long as the length parameter, your method should return the original string. For example, padString("congratulations", 10) would return "congratulations". */
+
+
+
+  /*Write a method called vertical that accepts a String as its parameter and prints each letter of the string on separate lines. For example, a call of vertical("hey now") should produce the following output:
+  h
+  e
+  y
+
+  n
+  o
+  w
+  */
+
+
+
+  /*Write a method called printReverse that accepts a String as its parameter and prints the characters in opposite order. For example, a call of printReverse("hello there!"); should print the following output:
+
+  !ereht olleh
+  
+  If an empty string is passed, no output is produced. Your method should produce a complete line of output.*/
+
+
+
+/*
+Write a method called inputBirthday that accepts a Scanner for the console as a parameter and prompts the user to enter a month, day, and year of birth, then prints the birthdate in a suitable format. Here is an example dialogue with the user:
+
+On what day of the month were you born? 8
+What is the name of the month in which you were born? May
+During what year were you born? 1981
+You were born on May 8, 1981. You're mighty old!
+*/
+
+
+
+/*
+Write a method called processName that accepts a Scanner for the console as a parameter and that prompts the user to enter his or her full name, then prints the name in reverse order (i.e., last name, first name). You may assume that only a first and last name will be given. You should read the entire line of input at once with the Scanner and then break it apart as necessary. Here is a sample dialogue with the user:
+
+Please enter your full name: Sammy Jankis
+Your name in reverse order is Jankis, Sammy
+*/
+
+
+/*
+Write a method called compareToLlama that takes a String as an iput and then lets the user know how many characters longer or shorter than llama the input string is. Note that it is fine for you to assume the user will interpet negative numbers being shorter and positive being longer.
+*/
+
+
+/*
+Write a method called isLlama that takes a String as an iput and then returns true if the String is "Llama" or false otherwise
+*/
+
+}
